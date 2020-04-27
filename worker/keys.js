e4f329df5d8a29224477a8e31ea43ea7e84b180e
Kunwar <knir45@gmail.com>
2020-04-27 15:50:49 -0400

    initial commit

---
+++ worker/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT
+};
+
+// "scripts": {
+  //   "start": "node index.js",
+  //   "dev": "nodemon"
+  // }
